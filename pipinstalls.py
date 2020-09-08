144626f76a4d6514ae172f72fd4b55a96591bb3f
Kieronia <70661699+kieronia@users.noreply.github.com>
2020-09-08 20:58:35 +0100

    Create pipinstalls.py
    Running this will get the required modules for the program.
    
    Notice any errors? Make sure you tick path and have python 3.85.
    
    I design for windows so may not be able to support other systems...

---
+++ pipinstalls.py
@@ -0,0 +1,10 @@
+from os import system
+
+system('pip install discord')
+system('pip install asyncio')
+system('pip install smtplib')
+system('pip install datetime')
+system('pip install configparser')
+system('pip install colorama')
+system('pip install sys')
+
